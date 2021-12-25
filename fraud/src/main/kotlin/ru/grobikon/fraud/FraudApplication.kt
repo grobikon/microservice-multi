ff2dcb27a697c6b85d285abe44a044e72224b6ce
Образцов Григорий <grobikon@mail.ru>
2021-12-25 16:32:49 +0300

    Added EurekaClients

--- fraud/src/main/kotlin/ru/grobikon/fraud/FraudApplication.kt
+++ fraud/src/main/kotlin/ru/grobikon/fraud/FraudApplication.kt
@@ -2,8 +2,10 @@ package ru.grobikon.fraud
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
 
 @SpringBootApplication
+@EnableEurekaClient
 class FraudApplication
 
 fun main(args: Array<String>) {
