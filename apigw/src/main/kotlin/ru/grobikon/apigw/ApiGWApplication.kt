0c006ba8ce4cae0d5562b3b1e95e0243a8625d4d
Образцов Григорий <grobikon@mail.ru>
2021-12-26 02:35:07 +0300

    Added project Api Gateway

---
+++ apigw/src/main/kotlin/ru/grobikon/apigw/ApiGWApplication.kt
@@ -0,0 +1,13 @@
+package ru.grobikon.apigw
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableEurekaClient
+class ApiGWApplication
+
+fun main(args: Array<String>){
+    runApplication<ApiGWApplication>(*args)
+}
